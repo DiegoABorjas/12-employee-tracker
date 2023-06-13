72d1d2b8fb40788b1014e9c9b02a0d821f5d28f4
Diego Borjas <diegoborjas@gmail.com>
2023-06-12 23:13:42 -0500

    finalized project, added screenshot and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# 12-employee-tracker
+# SQL Challenge: Employee Tracker
+
+## Description
+Develop a command line application to manage a company's employee database. Technologies used for this project are Node.js, MySQL, and Inquierer to build the back end of the application
+
+## Here's what the application looks like:
+![Screenshot](./images/screenshot.jpg)
+
+## Video
+Here's a walkthrough of the application [Video](https://drive.google.com/file/d/1L1VVecyn5ftMQAFDT1pGCKO0kkcKFgWd/view)
+
+## Link to repository:
+[Github](https://github.com/DiegoABorjas/12-employee-tracker)
+
+
+## User Story
+```
+AS A business owner
+I WANT to be able to view and manage the departments, roles, and employees in my company
+SO THAT I can organize and plan my business
+```
+
+## Acceptance Criteria
+```
+GIVEN a command-line application that accepts user input
+WHEN I start the application
+THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+WHEN I choose to view all departments
+THEN I am presented with a formatted table showing department names and department ids
+WHEN I choose to view all roles
+THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+WHEN I choose to view all employees
+THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+WHEN I choose to add a department
+THEN I am prompted to enter the name of the department and that department is added to the database
+WHEN I choose to add a role
+THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+WHEN I choose to add an employee
+THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+WHEN I choose to update an employee role
+THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+```
+
+## Installation
+
+To install necessary dependencies, run:
+```
+npm i
+```
+## Usage
+
+Run node index.js to start running the application
+
+---
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at diegoborjas@gmail.com. You can find more
+of my work at [diegoaborjas](https://github.com/diegoaborjas)
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const inquirer = require('inquirer')
-const mysql = require('mysql2')
-
 const { question } = require('./utils/question')
 
 const Department = require('./lib/departments')
 const Role = require('./lib/roles')
 const Employee = require('./lib/employees')
 
+// Function to prompt the app main question
 function promptQuestions() {
     inquirer.prompt(question).then(function(answer) {
         return handleAnswers(answer)
@@ -14,43 +13,58 @@ function promptQuestions() {
 }
 
 async function handleAnswers(response) {
-    // IF the response is to view all employees use the Employees method to query them all.
+    // IF the response is to view all employees use the proper Employees method
     if (response.answer === 'View all Employees') {
         const employees = new Employee
         await employees.getEmployees()
         promptQuestions()
     }
 
+    // IF the response is to add an employee use the proper Employees method
     if (response.answer === 'Add Employee') {
         const employees = new Employee
         await employees.addEmployee()
         console.log('New Employee Added')
         promptQuestions()
     }
-
+    // IF the response is to add an employee use the proper Employees method
+    if (response.answer === 'Update Employee Role') {
+        const employees = new Employee
+        await employees.updateEmployeeRole()
+        console.log('Employee role updated')
+        promptQuestions()
+    }
+    // IF the response is to view all roles use the proper Roles method
     if (response.answer === 'View all Roles') {
         const roles = new Role
         await roles.getRoles()
         promptQuestions()
     }
+    // IF the response is to add a role use the proper Roles method
     if (response.answer === 'Add Role') {
         const roles = new Role
         await roles.addRole()
         promptQuestions()
     }
+
+    // IF the response is to view all departments use the proper Departments method
     if (response.answer === 'View all Departments') {
         const departments = new Department
         await departments.getDepartments()
         promptQuestions()
     }
+
+    // IF the response is to add a department use the proper Departments method
     if (response.answer === 'Add Department') {
         const departments = new Department
         await departments.addDepartment()
         promptQuestions()
     }
+    // If the response is Quit end the application
     else if (response.answer === 'Quit') {
         process.exit()
     }
 }
 
+// Initialize app
 promptQuestions()
